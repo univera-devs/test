c7fbb2c70babd97cdcac88fe0c5412fbeddbb4ed
mahdimirzakhani4 <mahdimirzakhani85@gmail.com>
2025-10-22 22:30:27 +0330

    final config api

--- frontend/API.js
+++ frontend/API.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    // headers: {
+    //     'Content-Type': 'application/json',
+    // },
 });
 
 export default API;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import API from '../API'
 
 function App() {
   const [count, setCount] = useState(0)
+
+
+  useEffect(() => {
+    const handle = async () => {
+      try {
+        const res = await API.get("/test")
+        console.log(res?.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    handle()
+  }, [])
+
 
   return (
     <>
